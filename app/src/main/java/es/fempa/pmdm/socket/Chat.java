b896d39339718ab658b2243284b0c8b62ecd0edd
Cristian <cristianperezsegura@gmail.com>
2017-12-06 04:46:52 +0100

    Mejorado el diseño

--- app/src/main/java/es/fempa/pmdm/socket/Chat.java
+++ app/src/main/java/es/fempa/pmdm/socket/Chat.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
@@ -19,7 +20,11 @@ import android.content.Intent;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.io.ByteArrayOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
@@ -39,30 +44,44 @@ public class Chat extends Activity {
     EditText messageArea;
     ScrollView scrollView;
     RelativeLayout layout_2;
+    Thread clientThread = null;
+
+    private String response="";
 
     private TextView text;
-    private int SERVERPORT = 1048;
-    private String SERVER_IP = "192.168.100.6";
+    private int SERVERPORT;
+    private String SERVER_IP;
     private Socket socket;
+    private BufferedReader in;
+    private PrintWriter out;
+
+    Handler updateConversationHandler;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
-        sendButton = (ImageView)findViewById(R.id.sendButton);
-        messageArea = (EditText)findViewById(R.id.messageArea);
+
+
+        sendButton = (ImageView) findViewById(R.id.sendButton);
+        messageArea = (EditText) findViewById(R.id.messageArea);
         layout = (LinearLayout) findViewById(R.id.layout1);
-        layout_2 = (RelativeLayout)findViewById(R.id.layout2);
-        scrollView = (ScrollView)findViewById(R.id.scrollView);
+        layout_2 = (RelativeLayout) findViewById(R.id.layout2);
+        scrollView = (ScrollView) findViewById(R.id.scrollView);
 
         Intent data = getIntent();
         String tipo = data.getStringExtra("tipo");
         SERVERPORT = data.getIntExtra("puerto", -1);
+        SERVER_IP = data.getStringExtra("ip");
         text = (TextView) findViewById(R.id.datos);
 
-        new Thread(new ClientThread()).start();
 
+
+        this.clientThread = new Thread(new Chat.ClientThread());
+        this.clientThread.start();
+    }
 /*
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -93,65 +112,70 @@ public class Chat extends Activity {
 
             }
         });*/
-    }
-
-    public void onClick(View view) {
-        String messageText = messageArea.getText().toString();
-
-        TextView textView = new TextView(Chat.this);
-        textView.setTextColor(getResources().getColor(R.color.negro));
-
-        textView.setText(messageText);
-
-        LinearLayout.LayoutParams lp2 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
-        lp2.weight = 1.0f;
-        lp2.setMargins(0,25,5,0);
 
 
-        lp2.gravity = Gravity.LEFT;
-        textView.setBackgroundResource(R.drawable.bubble2_whatsapp);
+                public void onClick(View view) {
+                    String messageText = messageArea.getText().toString();
+
+                    TextView textView = new TextView(Chat.this);
+                    textView.setTextColor(getResources().getColor(R.color.negro));
+                    textView.setPadding(25,18,20,0);
+                    textView.setText(messageText);
+
+                    LinearLayout.LayoutParams lp2 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+                    lp2.weight = 1.0f;
+                    lp2.setMargins(0,25,5,0);
 
 
-        lp2.gravity = Gravity.RIGHT;
-        textView.setBackgroundResource(R.drawable.bubble_whatsapp);
 
-        textView.setLayoutParams(lp2);
-        layout.addView(textView);
-        scrollView.fullScroll(View.FOCUS_DOWN);
-        messageArea.setText("");
-        try {
-            PrintWriter out = new PrintWriter(new BufferedWriter(
-                    new OutputStreamWriter(socket.getOutputStream())),
-                    true);
-            out.println(messageText);
-        } catch (UnknownHostException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 
-    class ClientThread implements Runnable {
+                    lp2.gravity = Gravity.RIGHT;
+                    textView.setBackgroundResource(R.drawable.burbuja_verde);
 
-        @Override
-        public void run() {
+                    textView.setLayoutParams(lp2);
+                    layout.addView(textView);
+                    //scrollView.fullScroll(View.FOCUS_DOWN);
+                    messageArea.setText("");
+                    try {
+                        PrintWriter out = new PrintWriter(new BufferedWriter(
+                                new OutputStreamWriter(socket.getOutputStream())),
+                                true);
+                        out.println(messageText);
+                    } catch (UnknownHostException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
 
-            try {
-                Log.e("cliente", "dentro");
-                InetAddress serverAddr = InetAddress.getByName(SERVER_IP);
+        class ClientThread implements Runnable {
 
-                socket = new Socket(serverAddr, SERVERPORT);
+            @Override
+            public void run() {
+                try{
+                    socket = new Socket(SERVER_IP , SERVERPORT);
 
-            } catch (UnknownHostException e1) {
-                e1.printStackTrace();
-            } catch (IOException e1) {
-                e1.printStackTrace();
+
+                } catch (UnknownHostException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
             }
 
+
         }
 
-    }
+
+
+
+
+
+
+
+
 
 }
--- app/src/main/java/es/fempa/pmdm/socket/ChatServidor.java
+++ app/src/main/java/es/fempa/pmdm/socket/ChatServidor.java
@@ -1,19 +1,32 @@
 package es.fempa.pmdm.socket;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.InetAddress;
 import java.net.NetworkInterface;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.util.Enumeration;
 
 public class ChatServidor extends Activity {
@@ -28,20 +41,76 @@ public class ChatServidor extends Activity {
 
     public static final int SERVERPORT = 4545;
 
+
+    LinearLayout layout;
+    ImageView sendButton;
+    EditText messageArea;
+    ScrollView scrollView;
+    RelativeLayout layout_2;
+    Socket socket;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chat_servidor);
+        setContentView(R.layout.activity_chat);
 
-        text = (TextView) findViewById(R.id.text2);
+        sendButton = (ImageView)findViewById(R.id.sendButton);
+        messageArea = (EditText)findViewById(R.id.messageArea);
+        layout = (LinearLayout) findViewById(R.id.layout1);
+        layout_2 = (RelativeLayout)findViewById(R.id.layout2);
+        scrollView = (ScrollView)findViewById(R.id.scrollView);
+
+        text = (TextView) findViewById(R.id.datos);
         text.setText(getIpAddress());
         updateConversationHandler = new Handler();
+
+
 
         this.serverThread = new Thread(new ServerThread());
         this.serverThread.start();
 
     }
+
+    public void onClick(View view) {
+        String messageText = messageArea.getText().toString();
+
+        TextView textView = new TextView(ChatServidor.this);
+        textView.setTextColor(getResources().getColor(R.color.negro));
+        textView.setPadding(25,18,20,0);
+        if(messageText != null) {
+            textView.setText(messageText);
+        }
+        LinearLayout.LayoutParams lp2 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        lp2.weight = 1.0f;
+        lp2.setMargins(0,25,5,0);
+
+
+
+
+
+        lp2.gravity = Gravity.RIGHT;
+        textView.setBackgroundResource(R.drawable.burbuja_verde);
+
+        textView.setLayoutParams(lp2);
+        layout.addView(textView);
+        //scrollView.fullScroll(View.FOCUS_DOWN);
+        messageArea.setText("");
+        try {
+            PrintWriter out = new PrintWriter(new BufferedWriter(
+                    new OutputStreamWriter(socket.getOutputStream())),
+                    true);
+            out.println(messageText);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
 
     @Override
     protected void onStop() {
@@ -125,7 +194,27 @@ public class ChatServidor extends Activity {
 
         @Override
         public void run() {
-            text.setText(text.getText().toString()+"Client Says: "+ msg + "\n");
+            //String messageText = messageArea.getText().toString();
+            TextView textView = new TextView(ChatServidor.this);
+            textView.setTextColor(getResources().getColor(R.color.negro));
+
+            textView.setPadding(25,18,20,0);
+
+            String mensaje = msg;
+
+            textView.setText(mensaje);
+            LinearLayout.LayoutParams lp2 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            lp2.weight = 1.0f;
+            lp2.setMargins(0,25,5,0);
+
+
+            lp2.gravity = Gravity.LEFT;
+            textView.setBackgroundResource(R.drawable.burbuja_blanca);
+
+            textView.setLayoutParams(lp2);
+            layout.addView(textView);
+            //scrollView.fullScroll(View.FOCUS_DOWN);
+            //messageArea.setText("");
         }
 
     }
@@ -147,6 +236,7 @@ public class ChatServidor extends Activity {
                     if (inetAddress.isSiteLocalAddress())
                     {
                         ip += "IP de Servidor: " + inetAddress.getHostAddress() + "\n";
+
                     }
 
                 }
@@ -159,4 +249,5 @@ public class ChatServidor extends Activity {
 
         return ip;
     }
+
 }
